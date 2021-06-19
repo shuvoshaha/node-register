45fed9dc823ecb6f8ad4aa1a537deb9f44f0c8df
SKS <shaha460@gmail.com>
2021-06-19 23:48:50 +0600

    set and register engine and partials files

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require("express")
 const app = express();
-const router = express.Router();
+const Router = require("./routers/router")
 const port = process.env.PORT || 8000;
 const hbs = require("hbs");
 const path = require("path");
 require("./db/db")
 
+// custom path
 const static_path = path.join(__dirname, "./public")
 const template_path = path.join(__dirname, "./template/views")
 const partials_path = path.join(__dirname, "./template/partials")
 
-app.use(router)
+// register router
+app.use(Router)
+
+// set template engine
 app.set("views engine", "hbs");
+app.set("view", template_path)
 
-
-app.set("view" )
+//patials folder register
+hbs.registerPartials(partials_path)
 
 router.get("/", async(req, res) =>{
  res.send("Hello this is for test");
