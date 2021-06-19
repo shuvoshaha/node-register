3906b7a8d4f6c55fb8eef933956379301da82103
SKS <shaha460@gmail.com>
2021-06-19 23:43:16 +0600

    init custom path

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require("express")
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 8000;
-const hbs = require("hbs")
-
+const hbs = require("hbs");
+const path = require("path");
 require("./db/db")
 
+const static_path = path.join(__dirname, "./public")
+const template_path = path.join(__dirname, "./template/views")
+const partials_path = path.join(__dirname, "./template/partials")
+
 app.use(router)
-app.set()
+app.set("views engine", "hbs");
+
+
+app.set("view" )
 
 router.get("/", async(req, res) =>{
  res.send("Hello this is for test");
