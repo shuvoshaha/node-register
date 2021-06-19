09972d9cdd5b8a8875915d52d9845eda6a995c9e
SKS <shaha460@gmail.com>
2021-06-20 00:05:49 +0600

    set all files

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 const express = require("express")
 const app = express();
-const Router = require("./routers/router")
-const port = process.env.PORT || 8000;
-const hbs = require("hbs");
-const path = require("path");
+// const router = require("./routers/router")
+const port = process.env.PORT || 8080;
+const path = require("path")
 require("./db/db")
+const hbs = require("hbs");
+const router = express.Router()
 
-// custom path
-const static_path = path.join(__dirname, "./public")
-const template_path = path.join(__dirname, "./template/views")
-const partials_path = path.join(__dirname, "./template/partials")
 
-// register router
-app.use(Router)
 
-// static file register
-app.use(express.static(static_path))
-
+const template_path = path.join(__dirname, "./templates/views")
+const partials_path = path.join(__dirname, "./templates/partials")
+console.log(template_path)
 // set template engine
-app.set("views engine", "hbs");
-app.set("view", template_path)
+app.set("view engine", "hbs");
+app.set("views", template_path)
 
 //patials folder register
 hbs.registerPartials(partials_path)
 
+// custom path
+const static_path = path.join(__dirname, "./public")
+console.log(static_path)
+
+
+// register router
+app.use(router)
+
+// static file register
+app.use(express.static(static_path))
+
+
+
 router.get("/", async(req, res) =>{
- res.send("Hello this is for test");
+ res.render("index")
 })
 
 app.listen(port, () =>{
--- routers/router.js
+++ routers/router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const RegisterDB = require("./models/model")
+const RegisterDB = require("../models/model")
+
 
 router.get("/", async(req, res) =>{
-    
+   res.render("index")
 })
 
 module.exports = router;
