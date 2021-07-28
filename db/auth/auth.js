c1390ff5659112dac4c1aae7cc5b7d35aa3b0d18
SKS <shaha460@gmail.com>
2021-07-28 21:27:57 +0600

    after login create and verify token that's called auth

--- db/auth/auth.js
+++ db/auth/auth.js
@@ -1,6 +1,14 @@
-const cookieparser = require("cookie-parser");
+const jwt = require("jsonwebtoken")
 const Register = require("../models/model")
 
 const auth = async(req, res, next) =>{
-    next()
+   try{
+       const token = req.cookies.jwt;
+       const verifyToken = jwt.verify(token, process.env.SECRETKEY)
+       next()
+   }
+
+   catch(err){
+       res.status(401).send(err)
+   }
 }
